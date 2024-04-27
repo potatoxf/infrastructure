d909ca2fe77fbb656c4f66227c371324a4603038
potatoxf <potato.xf@qq.com>
2024-04-27 14:16:46 +0800

    提交TimeTicker时间器
    Signed-off-by: potatoxf <potato.xf@qq.com>

--- src/main/java/potatoxf/infrastructure/tools/TimeTicker.java
+++ src/main/java/potatoxf/infrastructure/tools/TimeTicker.java
@@ -9,18 +9,27 @@ package potatoxf.infrastructure.tools;
  */
 @FunctionalInterface
 public interface TimeTicker {
-    /**
-     * 返回 {@link System#nanoTime} 读取当前数量。
-     */
-    static TimeTicker systemTicker() {
-        return TimeTickerOfSystem.INSTANCE;
-    }
+    TimeTicker DISABLE = new Disable(), SYSTEM_NANO_TIME = new Disable(), SYSTEM_MILLIS_TIME = new SystemMillisTime();
 
     /**
      * 始终返回 {@code 0} 的数量。
      */
     static TimeTicker disabledTicker() {
-        return TimeTickerOfDisable.INSTANCE;
+        return TimeTicker.DISABLE;
+    }
+
+    /**
+     * 返回 {@link System#nanoTime} 读取当前数量。
+     */
+    static TimeTicker systemTicker() {
+        return TimeTicker.SYSTEM_NANO_TIME;
+    }
+
+    /**
+     * 返回 {@link System#currentTimeMillis()} 读取当前数量。
+     */
+    static TimeTicker systemMillisTimeticker() {
+        return TimeTicker.SYSTEM_MILLIS_TIME;
     }
 
     /**
@@ -29,4 +38,34 @@ public interface TimeTicker {
      * @return 返回固定参考点以来经过的数量
      */
     long read();
+
+    class Disable implements TimeTicker {
+        private Disable() {
+        }
+
+        @Override
+        public long read() {
+            return 0L;
+        }
+    }
+
+    class SystemNanoTime implements TimeTicker {
+        private SystemNanoTime() {
+        }
+
+        @Override
+        public long read() {
+            return System.nanoTime();
+        }
+    }
+
+    class SystemMillisTime implements TimeTicker {
+        private SystemMillisTime() {
+        }
+
+        @Override
+        public long read() {
+            return System.currentTimeMillis();
+        }
+    }
 }
